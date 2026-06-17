f4a48a0f69ca302959d622d3dc2678ed00307818
GynoPyno <matreda@gmail.com>
2026-06-17 22:41:53 +0200

    Delete AI Wave Survival directory

--- AI Wave Survival/projectiles/FlayerSubMissile/FlayerSubMissile_script.lua
+++
@@ -1,6 +0,0 @@
-local TMissileAAProjectile = import("/lua/terranprojectiles.lua").TMissileAAProjectile
-
---- Terran Anti Air Missile
----@class TAAMissileFlayer01: TMissileAAProjectile
-FlayerSubMissile = ClassProjectile(TMissileAAProjectile) {}
-TypeClass = FlayerSubMissile
